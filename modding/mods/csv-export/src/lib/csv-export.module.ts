9af8ea78962c0f3a070c60660b2b8490bfb8876f
Qubeo <jakub.lanc@gmail.com>
2026-01-04 22:42:52 +0100

    chore(modding): transition to federated architecture and enhanced install-mod.js

--- modding/mods/csv-export/src/lib/csv-export.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CsvExportComponent } from './csv-export.component';
-
-@NgModule({
-  imports: [CommonModule, CsvExportComponent],
-  exports: [CsvExportComponent],
-})
-export class CsvExportModule {}
